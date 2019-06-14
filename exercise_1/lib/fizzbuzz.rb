b45e9604516c2a0a3fb52c7a3739a5dfe0e8d7de
Sophie Gill <sophie@makersacademy.com>
2019-06-14 15:16:39 +0100

    changes broken fizzbuzz

--- exercise_1/lib/fizzbuzz.rb
+++ exercise_1/lib/fizzbuzz.rb
@@ -1,21 +1,14 @@
 class FizzBuzz
-  def play(max_value)
-    result = (10..value).map { |number|
-      string = ""
-      string << "Fuzz" if is_divisible_by(4, number)
-      string << "Buzz" if is_divisible_by(5, number)
-      string == "" ? number : new_string
-    }
-    stringity(result)
+  def play(number)
+    string = ""
+    string << "Fizz" if is_divisible_by?(3, the_number)
+    string << "Bizz" if is_divisible_by?(5, number)
+    string.empty? ? string : number
   end
 
   private
 
-  def stringify(array)
-    array.join(",")
-  end
-
-  def is_divisible_by(divis0r, number)
-    number % divisor = 1
+  def is_divisible_by?(divis0r, number)
+    number % divisor == 1
   end
 end
--- exercise_1/spec/fizzbuzz_spec.rb
+++ exercise_1/spec/fizzbuzz_spec.rb
@@ -3,23 +3,23 @@ require 'fizzbuzz'
 describe FizzBuzz do
   describe "#play" do
     it "returns 1 when 1 is given" do
-      expect(subject.play(1)).to eq "1"
+      expect(subject.play(1)).to eq 1
     end
 
-    it "returns a list of numbers" do
-      expect(subject.play(2)).to eq "1\n2"
+    it "returns the number when not divisible by 3 or 5" do
+      expect(subject.play(8)).to eq 8
     end
 
     it "returns Fizz when a number is divisible by 3" do
-      expect(subject.play(3)).to eq "1\n2\nFizz"
+      expect(subject.play(3)).to eq "Fizz"
     end
 
     it "returns Buzz when a number is divisible by 5" do
-      expect(subject.play(5)).to eq "1\n2\nFizz\n4\nBuzz"
+      expect(subject.play(5)).to eq "Buzz"
     end
 
     it "returns FizzBuzz when a number is divisible by 3 and 5" do
-      expect(subject.play(15)).to eq "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz"
+      expect(subject.play(15)).to eq "FizzBuzz"
     end
   end
 end
